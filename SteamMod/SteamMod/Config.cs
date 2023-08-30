9a915296f28ecbeef2e7c432e5c2b699e1e9d26f
ЖуЛиК <127218087+Xromchik@users.noreply.github.com>
2023-08-31 00:44:19 +0300

    Add files via upload

---
+++ SteamMod/SteamMod/Config.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamMod
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+        [Description("The number of days you need to have on your account to enter the server (int)")]
+        public int MinDay { get; set; } = 7;
+        [Description("Text displayed when a player is kicked from the server (string)")]
+        public string KickTxt { get; set; } = "Sorry, your account is too few days old, you can not play on this server";
+    }
+}
---
+++ SteamMod/SteamMod/Plugin.cs
@@ -0,0 +1,70 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using HtmlAgilityPack;
+using System;
+
+namespace SteamMod
+{
+    class Sawa : Plugin<Config>
+    {
+        public static Sawa plugin;
+        public override string Prefix => "SteamMod";
+        public override string Name => "SteamMod";
+        public override string Author => "Julik";
+        public override System.Version Version { get; } = new System.Version(0, 0, 1);
+
+        public override void OnEnabled()
+        {
+            plugin = this;
+            Exiled.Events.Handlers.Player.Verified += SendingARequest;
+            base.OnEnabled();
+        }
+        private void SendingARequest(VerifiedEventArgs ev)
+        {
+            string x = ev.Player.UserId;
+            int x1 = x.Length - 6; // Удаляю приписку @steam
+            x = x.Remove(x1);
+
+            string steamIdValue = x;
+
+            string url = $"https://steamid.io/lookup/{steamIdValue}";
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            // Использую XPath для поиска нужного элемента
+            HtmlNode infoNode = doc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]/dl/dd[6]");
+            string infoText;
+            if (infoNode != null)
+            {
+                infoText = infoNode.InnerText;
+            }
+            else
+            {
+                infoText = "Jan 1, 2009";
+            }
+
+            // Считаю разницу между датами 
+            string dateInput = infoText;
+            var parsedDate = DateTime.Parse(dateInput);
+
+            DateTime now = DateTime.Now;
+
+            TimeSpan ts = now.Subtract(parsedDate);
+            double diff = ts.TotalDays;
+
+
+            if (diff < plugin.Config.MinDay)
+            {
+                ev.Player.Kick(plugin.Config.KickTxt);
+            }
+        }
+
+        public override void OnDisabled()
+        {
+            plugin = this;
+            Exiled.Events.Handlers.Player.Verified -= SendingARequest;
+            base.OnDisabled();
+        }
+    }
+}
